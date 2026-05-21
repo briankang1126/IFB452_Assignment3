fca1c5914a961888af8e3fefa608bd13c7d9bf43
Brian Kang <briankang1126@gmail.com>
2026-05-21 15:36:32 +1000

    Update function: DeviceRegistry.sol

--- contracts/DeviceRegistry.sol
+++ contracts/DeviceRegistry.sol
@@ -2,5 +2,38 @@
 pragma solidity ^0.8.0;
 
 contract DeviceRegistry{
-    
+    address public oem;
+
+    modifier onlyOEM() {
+        require(msg.sender == oem, "Only OEM can call this");
+        _;
+    }
+
+    function registerDevice() public onlyOEM {
+
+    }
+
+    function registerComponent() public onlyOEM {
+
+    }
+
+    function transferOwnership() public onlyOEM {
+
+    }
+
+    function decommission() public onlyOEM {
+
+    }
+
+    function deviceExists() public onlyOEM {
+
+    }
+
+    function componentExists() public onlyOEM {
+
+    }
+
+    function getDevice() public onlyOEM {
+        
+    }
 }
